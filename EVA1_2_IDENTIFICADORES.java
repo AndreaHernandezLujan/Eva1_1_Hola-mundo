7766baa2d06baad8351021d998a326ca5bed8ea8
AndreaHernandezLujan <159495006+AndreaHernandezLujan@users.noreply.github.com>
2024-02-09 21:35:47 -0700

    Add files via upload

---
+++ EVA1_2_IDENTIFICADORES.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva_2_identificadores;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class EVA_2_IDENTIFICADORES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //VARIABLES:
+        //TIPO DE DATO + NOMBRE DEL IDENTIFICADOR:
+        //Short int byte long -- enteros
+        //float double -- numeros con decimal
+        //char -- character -- caracteres
+        //boolean -- verdadero o falso
+        //string -- cadenas de texto
+        int edad; //declaracón de una variable
+        String nombre;
+        double salario;
+        
+        //ASIGNACIÓN DE VALORES:
+        //IDENTIFICADOR = VALOR;
+        //se coloca el valor 50 dentro de la variable edad
+        edad = 50;
+        nombre= "juan perez jolote";
+        salario = 123.3;
+        //COMO LOS USAMOS:
+        System.out.println(nombre);
+        System.out.println(edad);
+        System.out.println(salario);
+    }
+    
+}
