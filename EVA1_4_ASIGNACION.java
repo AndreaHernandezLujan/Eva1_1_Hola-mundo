ed91a8a91caba3936b5fd30266b1bee7856aab50
AndreaHernandezLujan <159495006+AndreaHernandezLujan@users.noreply.github.com>
2024-02-16 02:46:42 -0700

    Add files via upload

---
+++ EVA1_4_ASIGNACION.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_4_asignacion;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class EVA1_4_ASIGNACION {
+
+    public static void main(String[] args) {
+       int edad; // DECLARACIÓN
+        edad = 42; //ASIGNACIÓN -- UN SOLO = SIGNIFICA ASIGNAR
+        System.out.println ("Tu edad es");
+        System. out. println (edad);
+        
+        String nombre;// DECLARACIÓN
+        nombre = "Andrea Valeria Hernández Luján";
+        System.out.println ("Tu nombre es");
+        System.out.println (nombre);
+        
+        String carrera;// DECLARACIÓN
+        carrera = "Ingenieria en sistemas computacionales";
+        System.out.println ("Tu carrera es");
+        System.out.println (carrera);
+    }
+}
